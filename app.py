2edb77fb8ebc23af4f38a9bdd735cd7f2300a38c
Kovid Dhanwaria <kovid.upes28@gmail.com>
2026-09-12 01:12:20 +0530

    Enable CSRF protection
    Added CSRF protection and secret key configuration.

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+import os
+import secrets
+
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or secrets.token_hex(32)
+
+csrf = CSRFProtect(app)
+
 
 @app.route("/")
 def home():
     return "DevSecOps CI/CD Pipeline is Working!"
+
 
 if __name__ == "__main__":
     app.run()
