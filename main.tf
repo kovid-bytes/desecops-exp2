bdebddbb4245bb984ad83b9d13115c3498ce92c3
Kovid Dhanwaria <kovid.upes28@gmail.com>
2026-09-12 01:35:34 +0530

    Format main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
-  # Skip credential validation for plan/syntax checks in CI
+  region                      = "us-east-1"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
 }
 
-# Define an S3 Bucket for Infrastructure
 resource "aws_s3_bucket" "devsecops_bucket" {
   bucket = "devsecops-iac-demo-bucket-2025"
 
